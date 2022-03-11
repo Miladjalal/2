57e3e9fc64ec38e2ecb4fe2a51b49d529f3afb99
milad <miladjalali1384@gmail.com>
2022-03-11 18:18:41 +0800

    1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int sum=0;int number,x=0,min;
+
+Console.ForegroundColor=ConsoleColor.Blue;
+System.Console.WriteLine("plases enter nomre");
+number=Convert.ToInt32(Console.ReadLine());
+
+while(number !=0)
+{
+ sum=sum+number;
+ x++;
+System.Console.WriteLine("plases enter nomre");
+number=Convert.ToInt32(Console.ReadLine());
+}
+min=sum/x;
+System.Console.WriteLine("javab"+min);
